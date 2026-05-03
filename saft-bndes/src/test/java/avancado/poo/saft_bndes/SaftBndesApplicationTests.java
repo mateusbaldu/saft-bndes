e872d153c46c9073b71429d44048966db26eaa4d
Mateus Balduino <balduinomateus0@gmail.com>
2026-05-02 21:58:46 -0300

    feat: adiciona estrutura inicial do projeto, junto com suas dependencias e configuracoes basicas no application.properties

---
+++ saft-bndes/src/test/java/avancado/poo/saft_bndes/SaftBndesApplicationTests.java
@@ -0,0 +1,13 @@
+package avancado.poo.saft_bndes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaftBndesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
