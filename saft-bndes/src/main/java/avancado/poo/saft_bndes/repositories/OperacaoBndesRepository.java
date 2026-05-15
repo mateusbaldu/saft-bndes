5de06791c53a51d2e50e285741bff985d8028a2a
Mateus Balduino <balduinomateus0@gmail.com>
2026-05-14 21:18:43 -0300

    feat(api): adiciona use-case de consulta por estado

--- saft-bndes/src/main/java/avancado/poo/saft_bndes/repositories/OperacaoBndesRepository.java
+++ saft-bndes/src/main/java/avancado/poo/saft_bndes/repositories/OperacaoBndesRepository.java
@@ -1,9 +1,12 @@
 package avancado.poo.saft_bndes.repositories;
 
 import avancado.poo.saft_bndes.models.OperacaoBndes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OperacaoBndesRepository extends JpaRepository<OperacaoBndes, Long> {
+    Page<OperacaoBndes> findByEstado(String estado, Pageable pageable);
 }
