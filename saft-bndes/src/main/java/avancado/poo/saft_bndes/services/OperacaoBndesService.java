57e6277d080708894adf9c5da09cd750cb1417aa
Mateus Balduino <balduinomateus0@gmail.com>
2026-05-15 21:56:47 -0300

    feat(api): adiciona use case de consulta de soma total de financiamentos por estado

--- saft-bndes/src/main/java/avancado/poo/saft_bndes/services/OperacaoBndesService.java
+++ saft-bndes/src/main/java/avancado/poo/saft_bndes/services/OperacaoBndesService.java
@@ -14,12 +14,12 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class OperacaoBndesService {
@@ -107,4 +107,15 @@ public class OperacaoBndesService {
                         operacao.getData()
                 ));
     }
+
+    public Map<String, BigDecimal> totalPorEstado() {
+        return repository.somarFinanciamentoPorEstado().stream()
+                .collect(Collectors.toMap(
+                        row -> (String) row[0],
+                        row -> (BigDecimal) row[1],
+                        (v1, v2) -> v1,
+                        LinkedHashMap::new
+                ));
+    }
 }
+
